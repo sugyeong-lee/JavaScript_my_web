6f78610bc8f7dfa69fd87a407e4408f3c80676f8
sugyeong-lee <waterscape2021@naver.com>
2022-08-25 19:41:56 +0900

    22/08/25-2

--- My_web/2_2_script.js
+++ My_web/2_2_script.js
@@ -1,10 +1,9 @@
 const numbersDiv = document.querySelector(".numbers")
-const drawButton = document.querySelector("#draw")
+const Button1 = document.querySelector("#5000")
+const Button2 = document.querySelector("#10000")
 const resetButton = document.querySelector("#reset")
-const numbersDiv2 = document.querySelector(".numbers2")
 
 const lottoNumbers = []
-const lottoNumbers2 = []
 const colors = ["tomato", "teal", "orange", "purple", "blue"]
 
 function paintNumber(number){
@@ -16,16 +15,7 @@ function paintNumber(number){
     numbersDiv.appendChild(eachNumDiv)
 }
 
-function paintNumber2(number){
-    const eachNumDiv2 = document.createElement("div")
-    let colorIndex = Math.floor(number/10)
-    eachNumDiv2.classList.add('eachnum')
-    eachNumDiv2.style.backgroundColor = colors[colorIndex]
-    eachNumDiv2.textContent = number
-    numbersDiv2.appendChild(eachNumDiv2)
-}
-
-drawButton.addEventListener("click", function(){
+Button1.addEventListener("click", function(){
     while(lottoNumbers.length < 6){
         let ran = Math.floor(Math.random()*45)+1
         if(lottoNumbers.indexOf(ran)===-1){
@@ -33,22 +23,9 @@ drawButton.addEventListener("click", function(){
             paintNumber(ran)
         }
     }
-    while(lottoNumbers2.length < 1){
-        let ran2 = Math.floor(Math.random()*45)+1
-        if(lottoNumbers2.indexOf(ran2)===-1){
-            lottoNumbers2.push(ran2)
-            paintNumber2(ran2)
-        }
-    }
-    var audio = new Audio("드럼.mp3");
-    audio.currentTime = 1;
-    audio.play();
-
 })
 
 resetButton.addEventListener("click", function(){
     lottoNumbers.splice(0,6)
-    lottoNumbers2.splice(0,6)
     numbersDiv.innerHTML = ""
-    numbersDiv2.innerHTML = ""
 })
