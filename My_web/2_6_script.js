0ee91d201f89212b80fc13b9366f274799867f2c
sugyeong-lee <waterscape2021@naver.com>
2022-09-01 19:00:14 +0900

    22/09/01

--- My_web/2_6_script.js
+++ My_web/2_6_script.js
@@ -6,8 +6,11 @@ const appendSeconds = document.getElementById("seconds");
 const appendMinutes = document.getElementById("minutes");
 const buttonStart = document.getElementById("bt__start");
 const buttonStop = document.getElementById("bt__stop");
+const buttonRecord = document.getElementById("bt_record");
 const buttonReset = document.getElementById("bt__reset");
+
 let intervalId;
+let records = "";
 
 buttonStart.onclick = function(){
     clearInterval(intervalId)
@@ -18,12 +21,24 @@ buttonStop.onclick = function(){
     clearInterval(intervalId)
 }
 
+buttonRecord.onclick = function(){
+    lastRec = document.getElementById("time").innerText;
+    document.getElementById("Last").innerText = lastRec;
+  
+    records += document.getElementById("time").innerText + "\n";
+    document.getElementById("List").innerText = records;
+  }
+
 buttonReset.onclick = function(){
     clearInterval(intervalId)
     tenMillis = 0; seconds = 0; minutes = 0;
     appendTens.textContent = "00"
     appendSeconds.textContent = "00"
     appendMinutes.textContent = "00"
+
+    records = "";
+    document.getElementById("lastRecord").innerText = "";
+    document.getElementById("recordList").innerText = "";
 }
 
 // 10ms 마다 시간에 대한 숫자가 증가한다.
